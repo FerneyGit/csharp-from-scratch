ccf4332e06b9d0c38c698456723b67685dc68ad5
Dafer <Dafer@FERNE>
2026-04-16 20:16:48 -0500

    Change Hello World by class

--- Hiworld/Program.cs
+++ Hiworld/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadLine();
+﻿namespace HiWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            var Name = "Ferney";
+            Name = "Ferney Esteban";
+            Console.WriteLine($"Hi World! {Name}");
+            Console.WriteLine();
+        }
+    }
+}
